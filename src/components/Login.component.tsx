ecdeaf3aedfbd5531f91f49fecac680111ce0d3d
Davi Batista <davidedeus28@hotmail.com>
2022-02-25 09:02:30 -0300

    Adding the outline of the validator

--- src/components/Login.component.tsx
+++ src/components/Login.component.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Input, Button, Label, Forms, Title } from './Login.component.style';
+import Validator from './data_validator';
 
-export default function Login():JSX.Element {
+export default function Login(): JSX.Element {
   return (
-    <Forms className='App'>
+    <Forms >
       <Title>Bem vindo(a) à Taqtile!</Title>
-      <Label > Email: </Label>
-      <Input type='Email' />
-      <Label > Senha: </Label>
-      <Input type='Senha' />
-      <Button>Submit</Button>
+      <Label> Email: </Label>
+      <Input id='email' type='Email' placeholder='email@example.com' />
+      <Label> Senha: </Label>
+      <Input id='senha' type='Senha' />
+      <Button onSubmit={Validator}>Submit</Button>
     </Forms>
   );
 }
---
+++ src/components/data_validator.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Validator() {
+  const input = document.querySelector('#email');
+  const email = input
+  console.log(email);
+}
