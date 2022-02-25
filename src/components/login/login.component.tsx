9faf42a2b153fda36889639e1e0fc8c74cc9cfa6
Davi Batista <davidedeus28@hotmail.com>
2022-02-25 12:21:06 -0300

    Adding the validation on the filds

--- src/components/login/login.component.tsx
+++ src/components/login/login.component.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input, Button, Label, Forms, Title } from './login.component.style';
 
 export function Login(): JSX.Element {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Forms>
       <Title>Bem vindo(a) à Taqtile!</Title>
       <Label> Email: </Label>
-      <Input id='email' type='Email' placeholder='email@example.com' />
+      <Input
+        id='email'
+        type='email'
+        required
+        placeholder='email@example.com'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        pattern="/^[a-zA-Z0-9.!#$%&'*+\=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+      />
       <Label> Senha: </Label>
-      <Input id='senha' type='Senha' />
+      <Input
+        id='senha'
+        type='password'
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        pattern='(^(?=.*\d)(?=.*[a-zA-Z]).{7,}$)'
+      />
       <Button>Submit</Button>
+      <div>
+        <p>
+          {email}
+          <br />
+          {password}
+        </p>
+      </div>
     </Forms>
   );
 }
